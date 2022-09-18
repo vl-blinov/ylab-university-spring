b27b9cb5fc59a66098225f01ea79533fe597b06c
vl-blinov <blinov.va93@gmail.com>
2022-09-18 22:15:51 +0300

    Update services

--- src/main/java/com/edu/ulab/app/service/impl/BookServiceImpl.java
+++ src/main/java/com/edu/ulab/app/service/impl/BookServiceImpl.java
@@ -1,31 +1,47 @@
 package com.edu.ulab.app.service.impl;
 
+import com.edu.ulab.app.dao.impl.BookDao;
 import com.edu.ulab.app.dto.BookDto;
+import com.edu.ulab.app.entity.Book;
+import com.edu.ulab.app.exception.NotFoundException;
+import com.edu.ulab.app.mapper.BookMapper;
 import com.edu.ulab.app.service.BookService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class BookServiceImpl implements BookService {
+
+    private BookDao bookDao;
+    private BookMapper bookMapper;
+
     @Override
     public BookDto createBook(BookDto bookDto) {
-        bookDto.setId(22L);
-        return bookDto;
+        Book bookEntity = bookMapper.bookDtoToBookEntity(bookDto);
+        Book createdBook = bookDao.add(bookEntity);
+        return bookMapper.bookEntityToBookDto(createdBook);
     }
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        return null;
+        Book bookEntity = bookMapper.bookDtoToBookEntity(bookDto);
+        Book updatedBookEntity = bookDao.update(bookEntity);
+        return bookMapper.bookEntityToBookDto(updatedBookEntity);
     }
 
     @Override
     public BookDto getBookById(Long id) {
-        return null;
+        Book extractedBookEntity = bookDao.findById(id)
+                .orElseThrow(() -> new NotFoundException("Book with id '" + id + "' is not found"));
+        return bookMapper.bookEntityToBookDto(extractedBookEntity);
     }
 
     @Override
     public void deleteBookById(Long id) {
-
+        getBookById(id);
+        bookDao.deleteById(id);
     }
 }
--- src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
@@ -1,34 +1,47 @@
 package com.edu.ulab.app.service.impl;
 
+import com.edu.ulab.app.dao.impl.UserDao;
 import com.edu.ulab.app.dto.UserDto;
+import com.edu.ulab.app.entity.User;
+import com.edu.ulab.app.exception.NotFoundException;
+import com.edu.ulab.app.mapper.UserMapper;
 import com.edu.ulab.app.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+    private UserMapper userMapper;
+
     @Override
     public UserDto createUser(UserDto userDto) {
-        // сгенерировать идентификатор
-        // создать пользователя
-        // вернуть сохраненного пользователя со всеми необходимыми полями id
-        userDto.setId(1L);
-        return userDto;
+        User newUserEntity = userMapper.userDtoToUserEntity(userDto);
+        User createdUserEntity = userDao.add(newUserEntity);
+        return userMapper.userEntityToUserDto(createdUserEntity);
     }
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        return null;
+        User updatingUserEntity = userMapper.userDtoToUserEntity(userDto);
+        User updatedUserEntity = userDao.update(updatingUserEntity);
+        return userMapper.userEntityToUserDto(updatedUserEntity);
     }
 
     @Override
     public UserDto getUserById(Long id) {
-        return null;
+        User extractedUserEntity = userDao.findById(id)
+                .orElseThrow(() -> new NotFoundException("User with id '" + id + "' is not found"));
+        return userMapper.userEntityToUserDto(extractedUserEntity);
     }
 
     @Override
     public void deleteUserById(Long id) {
-
+        getUserById(id);
+        userDao.deleteById(id);
     }
 }
