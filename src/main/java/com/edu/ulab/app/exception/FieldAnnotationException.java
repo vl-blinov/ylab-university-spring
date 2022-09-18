4dcddd5c8f765f20f37703a87507844ce6e517d6
vl-blinov <blinov.va93@gmail.com>
2022-09-18 22:12:19 +0300

    Create FieldAnnotationException

---
+++ src/main/java/com/edu/ulab/app/exception/FieldAnnotationException.java
@@ -0,0 +1,7 @@
+package com.edu.ulab.app.exception;
+
+public class FieldAnnotationException extends IllegalStateException {
+    public FieldAnnotationException(String message) {
+        super(message);
+    }
+}
