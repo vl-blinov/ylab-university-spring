8478dcb3e4c1d4b684b501d964b3bd5f0401732b
vl-blinov <blinov.va93@gmail.com>
2022-09-26 00:57:14 +0300

    Update UserServiceImpl and BookServiceImpl

--- src/main/java/com/edu/ulab/app/service/impl/BookServiceImpl.java
+++ src/main/java/com/edu/ulab/app/service/impl/BookServiceImpl.java
@@ -1,47 +1,60 @@
 package com.edu.ulab.app.service.impl;
 
-import com.edu.ulab.app.dao.impl.BookDao;
 import com.edu.ulab.app.dto.BookDto;
 import com.edu.ulab.app.entity.Book;
 import com.edu.ulab.app.exception.NotFoundException;
 import com.edu.ulab.app.mapper.BookMapper;
+import com.edu.ulab.app.mapper.CycleAvoidingMappingContext;
+import com.edu.ulab.app.repository.BookRepository;
 import com.edu.ulab.app.service.BookService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@Qualifier("bookServiceJpa")
 @AllArgsConstructor
 public class BookServiceImpl implements BookService {
 
-    private BookDao bookDao;
+    private BookRepository bookRepository;
     private BookMapper bookMapper;
 
     @Override
     public BookDto createBook(BookDto bookDto) {
-        Book bookEntity = bookMapper.bookDtoToBookEntity(bookDto);
-        Book createdBook = bookDao.add(bookEntity);
-        return bookMapper.bookEntityToBookDto(createdBook);
+        Book book = bookMapper.bookDtoToBookEntity(bookDto, new CycleAvoidingMappingContext());
+        log.info("Mapped book: {}", book);
+        Book createdBook = bookRepository.save(book);
+        log.info("Created book: {}", createdBook);
+        return bookMapper.bookEntityToBookDto(createdBook, new CycleAvoidingMappingContext());
     }
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        Book bookEntity = bookMapper.bookDtoToBookEntity(bookDto);
-        Book updatedBookEntity = bookDao.update(bookEntity);
-        return bookMapper.bookEntityToBookDto(updatedBookEntity);
+        Book book = bookMapper.bookDtoToBookEntity(bookDto, new CycleAvoidingMappingContext());
+        log.info("Mapped book: {}", book);
+        Book updatedBook = bookRepository.save(book);
+        log.info("Updated book: {}", updatedBook);
+        return bookMapper.bookEntityToBookDto(updatedBook, new CycleAvoidingMappingContext());
     }
 
     @Override
     public BookDto getBookById(Long id) {
-        Book extractedBookEntity = bookDao.findById(id)
-                .orElseThrow(() -> new NotFoundException("Book with id '" + id + "' is not found"));
-        return bookMapper.bookEntityToBookDto(extractedBookEntity);
+        Book extractedBook = extractBookEntity(id);
+        log.info("Extracted book: {}", extractedBook);
+        return bookMapper.bookEntityToBookDto(extractedBook, new CycleAvoidingMappingContext());
     }
 
     @Override
     public void deleteBookById(Long id) {
-        getBookById(id);
-        bookDao.deleteById(id);
+        Book extractedBook = extractBookEntity(id);
+        extractedBook.getUser().removeBook(extractedBook);
+        bookRepository.deleteById(id);
+    }
+
+    private Book extractBookEntity(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Book with id '" + id + "' is not found"));
     }
 }
