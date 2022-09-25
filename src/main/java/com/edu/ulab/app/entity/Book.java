c39364f81ddac4c6e2f97bc30fc1b15e2d00dc8f
vl-blinov <blinov.va93@gmail.com>
2022-09-26 00:34:16 +0300

    Update Book.java

--- src/main/java/com/edu/ulab/app/entity/Book.java
+++ src/main/java/com/edu/ulab/app/entity/Book.java
@@ -1,25 +1,41 @@
 package com.edu.ulab.app.entity;
 
-import com.edu.ulab.app.annotations.AutoIncrementedId;
-import com.edu.ulab.app.annotations.ManyToOneRelationship;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Entity
 public class Book {
 
-    @AutoIncrementedId
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOneRelationship
-    private User user;
-
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String author;
 
+    @NotNull
     private long pageCount;
+
+    @ToString.Exclude
+    @ManyToOne(cascade = {CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.REFRESH})
+    @JoinColumn
+    private Person user;
+
+    public Book(String title, String author, long pageCount) {
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+    }
 }
