ec0de9efb821e9465d629e8f45fde6bc8ca34ec0
vl-blinov <blinov.va93@gmail.com>
2022-10-07 18:03:29 +0300

    Update BookServiceImplTest

--- src/test/java/com/edu/ulab/app/service/BookServiceImplTest.java
+++ src/test/java/com/edu/ulab/app/service/BookServiceImplTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
- * Тестирование функционала {@link BookServiceImpl}.
+ * Functionality tests {@link BookServiceImpl}.
  */
 @UnitTest
 public class BookServiceImplTest {
